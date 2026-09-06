333255605e448155a32fb8948143c32e6cc9313b
amrishdwivediji <97836559+AMRISHKUMARDWIVEDI08@users.noreply.github.com>
2026-09-06 16:11:29 +0530

    chore: remove unused Circle API route

--- backend/routes/api/v1/circle.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const { Router } = require('express');
-const { authenticate } = require('../../../middleware/auth');
-const { requireRole } = require('../../../middleware/roleGuard');
-const controller = require('../../../controllers/circleConfigController');
-const { ROLES } = require('../../../config/constants');
-const router = Router();
-router.get('/status', authenticate, requireRole(ROLES.ADMIN), controller.getCircleStatus);
-module.exports = router;
